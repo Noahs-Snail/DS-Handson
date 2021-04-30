6aa59c1c21ec3bee72880b900513386206d291f2
Karishma R <61197280+Noahs-Snail@users.noreply.github.com>
2021-04-30 23:25:58 +0530

    Add files via upload

---
+++ Observer Pattern/src/MessagePublisher.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessagePublisher extends Subject{
+	private List<Observer> observers = new ArrayList<>();
+
+	public void attach(Observer o) {
+
+	observers.add(o);
+
+	}
+
+	public void detach(Observer o)
+	{
+		observers.remove(o); 
+	} 
+	public void notifyUpdate(Message m){
+		for(Observer o: observers) {
+		o.update(m); 
+		}
+	}
+
+}
